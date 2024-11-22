090dd739f4b1ea770589d5e9ed655d7aa9481f19
Eduardos13 <edik.kozelsky@gmail.com>
2024-11-21 22:23:31 -0500

    some base configs

---
+++ src/server.js
@@ -0,0 +1,39 @@
+import express from 'express';
+import pino from 'pino-http';
+import cors from 'cors';
+import { env } from './utils/env.js';
+import { ENV_VARS } from './constants/index.js';
+import errorHandlerMiddleware from './middlewares/errorHandler.js';
+import notFoundMiddleware from './middlewares/notFound.js';
+
+const PORT = env(ENV_VARS.PORT, 3000);
+
+export const startServer = () => {
+  const app = express();
+
+  app.use(
+    pino({
+      transport: {
+        target: 'pino-pretty',
+      },
+    }),
+  );
+
+  app.use(cors());
+
+  app.use(express.json());
+
+  app.get('/', (req, res) => {
+    res.json({
+      message: 'Home page',
+    });
+  });
+
+  app.use('*', notFoundMiddleware);
+
+  app.use(errorHandlerMiddleware);
+
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+};
