01891851ef788d4ad3b276a0b1db7203206d4c92
Eduardos13 <edik.kozelsky@gmail.com>
2024-11-23 21:00:41 -0500

    Connected MongoDB

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,3 +1,11 @@
 export const ENV_VARS = {
   PORT: 'PORT',
 };
+
+export const MONGO_DB_VARS = {
+  PORT: 'PORT',
+  MONGO_USER: 'MONGO_USER',
+  MONGO_PASSWORD: 'MONGO_PASSWORD',
+  MONGO_URL: 'MONGO_URL',
+  MONGO_DB: 'MONGO_DB',
+};
